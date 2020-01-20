9faf21dcdb169a76bcd9f4e7ca1fa4004a3457c9
Don Henton <donhenton@gmail.com>
2020-01-20 10:38:34 -0600

    updating to working status

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # github-codedeploy
 
-Sample revision. zz.sh is the push command
+This is a repository containing the deploy bundle for the udemy class < https://udemy.com/course/aws-codedeploy>.
+
+## Notes
+
+The class is out of date, and the start and stop scripts had to be updated for the specific AMI that was chosen
+See the Cloudformation scripts at <https://github.com/donhenton/codedeploy-class>
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-
+# this will have to have the eTag changed with the value returned by running push
 
 # To deploy with this revision, run:
 aws deploy create-deployment --application-name DemoApp \
- --s3-location bucket=com.awsdhenton.deploy-bucket,key=LinuxWeb.zip,bundleType=zip,eTag=d1cc4cfb4bcf720734f05bb2879e95d2 \
+ --s3-location bucket=com.awsdhenton.deploy-bucket,key=LinuxWeb.zip,bundleType=zip,eTag=5519035b8fde50680f5bf48f7af422b2  \
  --deployment-group-name LinuxWebTier \
  --description 'simple deploy for linux' \
  --profile cdktest
